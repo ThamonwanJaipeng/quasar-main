63f66f6fb79d2f3de312e436257c67f8a59fa5e8
ThamonwanJaipeng <thamonwan20041025@gmail.com>
2024-07-31 10:40:33 +0700

    modified id-code

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -6,6 +6,15 @@
       @reset="onReset"
       class="q-gutter-md"
     >
+     <!-- id -->
+     <q-input
+        filled
+        v-model="id"
+        label="Your id *"
+        hint="id-code"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Please type your id-code']"
+      />
      <!-- name -->
       <q-input
         filled
@@ -56,12 +65,14 @@ import { ref } from 'vue'
 export default {
   setup () {
     const $q = useQuasar()
-
-    const name = ref(ธมลวรรณ)
-    const surname = ref(ใจเป็ง)
+    const id = ref("6604101334")
+    const name = ref("ธมลวรรณ")
+    const surname = ref("ใจเป็ง")
+    const age = ref(null)
     const accept = ref(false)
 
     return {
+      id,
       name,
       surname,
       accept,
